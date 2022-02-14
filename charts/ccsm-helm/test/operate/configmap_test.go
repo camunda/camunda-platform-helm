d0dd617d46a8d6e8d52f702e68e092a2a2d7c75c
Christopher Zell <zelldon91@googlemail.com>
2022-02-14 09:14:01 +0100

    test: add first operate golden test

---
+++ charts/ccsm-helm/test/operate/configmap_test.go
@@ -0,0 +1,64 @@
+package test
+
+import (
+	"flag"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+var update = flag.Bool("update-golden", false, "update golden test output files")
+
+type configmapTemplateTest struct {
+	suite.Suite
+	chartPath string
+	release   string
+	namespace string
+	templates []string
+}
+
+func TestConfigmapTemplate(t *testing.T) {
+	t.Parallel()
+
+	chartPath, err := filepath.Abs("../../")
+	require.NoError(t, err)
+
+	suite.Run(t, &configmapTemplateTest{
+		chartPath: chartPath,
+		release:   "ccsm-helm-test",
+		namespace: "ccsm-helm-" + strings.ToLower(random.UniqueId()),
+		templates: []string{"charts/operate/templates/configmap.yaml"},
+	})
+}
+
+func (s *configmapTemplateTest) TestContainerGoldenTestConfigMapDefaults() {
+	options := &helm.Options{
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+	}
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+	actual := []byte(output)
+
+	goldenFile := "configmap.golden.json"
+
+	if *update {
+		err := ioutil.WriteFile(goldenFile, actual, 0644)
+		if err != nil {
+			return
+		}
+	}
+
+	expected, err := ioutil.ReadFile(goldenFile)
+	if err != nil {
+		s.Fail("Golden file doesn't exist or was not readable", err)
+		return
+	}
+
+	s.Require().Equal(string(expected), output)
+}
