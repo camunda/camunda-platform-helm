3be6b619e9126b12085fb0415f5efcc5c1fca2cd
Christopher Zell <zelldon91@googlemail.com>
2022-02-16 12:42:52 +0100

    test: add golden tests for tasklist

---
+++ charts/ccsm-helm/test/tasklist/goldenfiles_test.go
@@ -0,0 +1,31 @@
+package operate
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"camunda-cloud-helm/charts/ccsm-helm/test/golden"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestGoldenDefaultsTemplate(t *testing.T) {
+	t.Parallel()
+
+	chartPath, err := filepath.Abs("../../")
+	require.NoError(t, err)
+	templateNames := []string{"service", "deployment", "configmap"}
+
+	for _, name := range templateNames {
+		suite.Run(t, &golden.TemplateGoldenTest{
+			ChartPath:      chartPath,
+			Release:        "ccsm-helm-test",
+			Namespace:      "ccsm-helm-" + strings.ToLower(random.UniqueId()),
+			GoldenFileName: name,
+			Templates:      []string{"charts/tasklist/templates/" + name + ".yaml"},
+		})
+	}
+}
