17fddab2ffc326c4fbcae694df72e1d6ed3f02fc
Christopher Zell <zelldon91@googlemail.com>
2022-02-23 14:47:35 +0100

    test: add additional tests
     * Test for priority class name
     * Test for image pull secrets
     * Test for init containers and templating values

--- charts/ccsm-helm/test/zeebe/statefulset_test.go
+++ charts/ccsm-helm/test/zeebe/statefulset_test.go
@@ -54,6 +54,71 @@ func (s *statefulSetTest) TestContainerSetPodLabels() {
 	s.Require().Equal("bar", statefulSet.Spec.Template.Labels["foo"])
 }
 
+func (s *statefulSetTest) TestContainerSetPriorityClassName() {
+	// given
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"zeebe.priorityClassName": "PRIO",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+	}
+
+	// when
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+	var statefulSet v1.StatefulSet
+	helm.UnmarshalK8SYaml(s.T(), output, &statefulSet)
+
+	// then
+	s.Require().Equal("PRIO", statefulSet.Spec.Template.Spec.PriorityClassName)
+}
+
+func (s *statefulSetTest) TestContainerSetImagePullSecrets() {
+	// given
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"global.image.pullSecrets[0].name": "SecretName",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+	}
+
+	// when
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+	var statefulSet v1.StatefulSet
+	helm.UnmarshalK8SYaml(s.T(), output, &statefulSet)
+
+	// then
+	s.Require().Equal("SecretName", statefulSet.Spec.Template.Spec.ImagePullSecrets[0].Name)
+}
+
+func (s *statefulSetTest) TestContainerSetExtraInitContainers() {
+	// given
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"zeebe.extraInitContainers[0].name": "init-container-{{ .Release.Name }}",
+			"zeebe.extraInitContainers[0].image": "busybox:1.28",
+			"zeebe.extraInitContainers[0].command[0]": "sh",
+			"zeebe.extraInitContainers[0].command[1]": "-c",
+			"zeebe.extraInitContainers[0].command[2]": "top",
+			"zeebe.extraInitContainers[0].volumeMounts[0].name": "exporters",
+			"zeebe.extraInitContainers[0].volumeMounts[0].mountPath": "/exporters/",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+	}
+
+	// when
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+	var statefulSet v1.StatefulSet
+	helm.UnmarshalK8SYaml(s.T(), output, &statefulSet)
+
+	// then
+	initContainer := statefulSet.Spec.Template.Spec.InitContainers[0]
+	s.Require().Equal("init-container-ccsm-helm-test", initContainer.Name)
+	s.Require().Equal("busybox:1.28", initContainer.Image)
+	s.Require().Equal([]string{"sh", "-c", "top"}, initContainer.Command)
+	s.Require().Equal("exporters", initContainer.VolumeMounts[0].Name)
+	s.Require().Equal("/exporters/", initContainer.VolumeMounts[0].MountPath)
+}
+
 func TestGoldenContainerSecurityContext(t *testing.T) {
 	t.Parallel()
 
