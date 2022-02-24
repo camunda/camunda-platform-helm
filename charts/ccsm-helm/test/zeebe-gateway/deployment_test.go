a475f11266e0ca1957567acaf5844cd0979cfbad
Christopher Zell <zelldon91@googlemail.com>
2022-02-24 13:58:41 +0100

    test: set gateway image pull secrets

--- charts/ccsm-helm/test/zeebe-gateway/deployment_test.go
+++ charts/ccsm-helm/test/zeebe-gateway/deployment_test.go
@@ -71,6 +71,24 @@ func (s *deploymentTemplateTest) TestContainerSetPriorityClassName() {
 	s.Require().Equal("PRIO", deployment.Spec.Template.Spec.PriorityClassName)
 }
 
+func (s *deploymentTemplateTest) TestContainerSetImagePullSecrets() {
+	// given
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"global.image.pullSecrets[0].name": "SecretName",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+	}
+
+	// when
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+	var deployment appsv1.Deployment
+	helm.UnmarshalK8SYaml(s.T(), output, &deployment)
+
+	// then
+	s.Require().Equal("SecretName", deployment.Spec.Template.Spec.ImagePullSecrets[0].Name)
+}
+
 func (s *deploymentTemplateTest) TestContainerOverwriteImageTag() {
 	// given
 	options := &helm.Options{
