b5c97610115b73500ad7c4585f41341144005eb8
Christopher Zell <zelldon91@googlemail.com>
2022-06-10 15:23:30 +0200

    feat: add podAnnotations to optimize

--- charts/camunda-platform/test/optimize/deployment_test.go
+++ charts/camunda-platform/test/optimize/deployment_test.go
@@ -68,6 +68,26 @@ func (s *deploymentTemplateTest) TestContainerSetPodLabels() {
 	s.Require().Equal("bar", deployment.Spec.Template.Labels["foo"])
 }
 
+func (s *deploymentTemplateTest) TestContainerSetPodAnnotations() {
+	// given
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"optimize.podAnnotations.foo": "bar",
+			"optimize.podAnnotations.foz": "baz",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+	}
+
+	// when
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+	var deployment appsv1.Deployment
+	helm.UnmarshalK8SYaml(s.T(), output, &deployment)
+
+	// then
+	s.Require().Equal("bar", deployment.Spec.Template.Annotations["foo"])
+	s.Require().Equal("baz", deployment.Spec.Template.Annotations["foz"])
+}
+
 func (s *deploymentTemplateTest) TestContainerSetGlobalAnnotations() {
 	// given
 	options := &helm.Options{
