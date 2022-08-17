b2edb54f713436d3a853e7db186846618eb9236f
Ahmed AbouZaid <ahmed.abouzaid@camunda.com>
2022-08-17 21:04:58 +0200

    chore: automate the release notes
    for github and artifacts hub releases

---
+++ scripts/generate-release-notes.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -euo pipefail
+
+chart_files_to_release="${1:-''}"
+
+for chart_file in ${chart_files_to_release}; do
+    chart_name=$(grep -Po "(?<=^name: ).+" ${chart_file})
+    chart_version=$(grep -Po "(?<=^version: ).+" ${chart_file})
+    chart_tag="${chart_name}-${chart_version}"
+    chart_path="charts/${chart_name}"
+
+    #
+    # Generate RELEASE-NOTES.md file (used for Github release notes and ArtifactHub "changes" annotation).
+    git-chglog                                    \
+        --output "${chart_path}/RELEASE-NOTES.md" \
+        --tag-filter-pattern "${chart_name}"      \
+        --next-tag "${chart_tag}"                 \
+        --path "${chart_path}" "${chart_tag}"
+
+    #
+    # Update ArtifactHub "changes" annotation in the Chart.yaml file.
+    # https://artifacthub.io/docs/topics/annotations/helm/#supported-annotations
+    change_types="$(yq e '.options.commits.filters.Type | join(" ")' .chglog/config.yml)"
+
+    # 
+    declare -A kac_map
+    kac_map+=(
+        ["feat"]=added
+        ["refactor"]=changed
+        ["fix"]=fixed
+    )
+
+    echo '|' > "${chart_path}/changes-for-artifacthub.yaml.tmp"
+
+    for change_type in ${change_types}; do
+        change_type_section=$(sed -rn "/^\#+\s${change_type^}/,/^#/p" "${chart_path}/RELEASE-NOTES.md")
+        if [[ -n "${change_type_section}" && "${!kac_map[@]}" =~ "${change_type}" ]]; then
+            echo "${change_type_section}" | egrep '^\*' | sed 's/^* //g' | while read commit_message; do
+                echo "  - kind: ${kac_map[${change_type}]} "
+                echo "    description: \"${commit_message}\""
+            done >> "${chart_path}/changes-for-artifacthub.yaml.tmp"
+        fi
+    done
+
+    cat "${chart_path}/changes-for-artifacthub.yaml.tmp"
+
+    if [[ $(cat "${chart_path}/changes-for-artifacthub.yaml.tmp" | wc -l) -eq 1 ]]; then
+        echo "[ERROR] Somthing is wrong, no changes detected to generate Artifact Hub changelog."
+        exit 1
+    fi
+
+    # Merge changes back to the Chart.yaml file.
+    yq eval-all \
+        'select(fileIndex==0).annotations."artifacthub.io/changes" = select(fileIndex==1) | select(fileIndex==0)' \
+        ${chart_path}/Chart.yaml ${chart_path}/changes-for-artifacthub.yaml.tmp > \
+        ${chart_path}/Chart-with-artifacthub-changes.yaml.tmp
+    mv ${chart_path}/Chart-with-artifacthub-changes.yaml.tmp ${chart_path}/Chart.yaml
+done
