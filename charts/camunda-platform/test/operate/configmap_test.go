b272fc18d011bd2df8a27ebd5e3e27eeb43b90c2
Ahmed AbouZaid <ahmed.abouzaid@camunda.com>
2022-09-19 16:13:34 +0200

    test(operate): use same configmap test as tasklist

--- charts/camunda-platform/test/operate/configmap_test.go
+++ charts/camunda-platform/test/operate/configmap_test.go
@@ -15,29 +15,59 @@
 package operate
 
 import (
-	"camunda-platform-helm/charts/camunda-platform/test/golden"
 	"path/filepath"
 	"strings"
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	v1 "k8s.io/api/core/v1"
 )
 
-// TODO: Create a property-based test like we have in Tasklist.
-func TestGoldenConfigmapWithElasticsearchUrl(t *testing.T) {
+type configMapTemplateTest struct {
+	suite.Suite
+	chartPath string
+	release   string
+	namespace string
+	templates []string
+}
+
+func TestConfigMapTemplate(t *testing.T) {
 	t.Parallel()
 
 	chartPath, err := filepath.Abs("../../")
 	require.NoError(t, err)
 
-	suite.Run(t, &golden.TemplateGoldenTest{
-		ChartPath:      chartPath,
-		Release:        "camunda-platform-test",
-		Namespace:      "camunda-platform-" + strings.ToLower(random.UniqueId()),
-		GoldenFileName: "configmap-elastic-url",
-		Templates:      []string{"charts/operate/templates/configmap.yaml"},
-		SetValues:      map[string]string{"global.elasticsearch.url": "http://elasticmaster.org"},
+	suite.Run(t, &configMapTemplateTest{
+		chartPath: chartPath,
+		release:   "camunda-platform-test",
+		namespace: "camunda-platform-" + strings.ToLower(random.UniqueId()),
+		templates: []string{"charts/operate/templates/configmap.yaml"},
 	})
+}
+
+func (s *configMapTemplateTest) TestConfigMapElasticsearchURL() {
+	// given
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"global.elasticsearch.url": "elasticsearch-master-test",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+	}
+
+	// when
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+	var configmap v1.ConfigMap
+	var configmapApplication map[string]interface{}
+	helm.UnmarshalK8SYaml(s.T(), output, &configmap)
+	helm.UnmarshalK8SYaml(s.T(), configmap.Data["application.yml"], &configmapApplication)
+
+	// TODO: Move Operate config to its own struct when we have more tests.
+	elasticsearchURL := configmapApplication["camunda.operate"].(map[string]interface{})["elasticsearch"].(map[string]interface{})["url"]
+
+	// then
+	s.Require().Equal("elasticsearch-master-test", elasticsearchURL)
 }
