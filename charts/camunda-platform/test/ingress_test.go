b22f6922e5e6ab4e347268f164786045fcc604df
Ahmed AbouZaid <ahmed.abouzaid@camunda.com>
2022-10-24 17:22:13 +0200

    feat: allow to exclude components in combined the ingress

--- charts/camunda-platform/test/ingress_test.go
+++ charts/camunda-platform/test/ingress_test.go
@@ -34,7 +34,7 @@ func TestIngressTemplate(t *testing.T) {
 	})
 }
 
-func (s *ingressTemplateTest) TestIngress() {
+func (s *ingressTemplateTest) TestIngressWithContextPath() {
 	// given
 	options := &helm.Options{
 		SetValues: map[string]string{
@@ -58,4 +58,52 @@ func (s *ingressTemplateTest) TestIngress() {
 	s.Require().Contains(output, "path: /operate")
 	s.Require().Contains(output, "path: /optimize")
 	s.Require().Contains(output, "path: /tasklist")
+}
+
+func (s *ingressTemplateTest) TestIngressComponentWithNoContextPath() {
+	// given
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"global.ingress.enabled": "true",
+			"identity.contextPath":   "",
+			"operate.contextPath":    "",
+			"optimize.contextPath":   "",
+			"tasklist.contextPath":   "",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+		ExtraArgs:      map[string][]string{"template": {"--debug"}, "install": {"--debug"}},
+	}
+
+	// when
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+
+	// then
+	s.Require().NotContains(output, "name: camunda-platform-test-identity")
+	s.Require().NotContains(output, "name: camunda-platform-test-operate")
+	s.Require().NotContains(output, "name: camunda-platform-test-optimize")
+	s.Require().NotContains(output, "name: camunda-platform-test-tasklist")
+}
+
+func (s *ingressTemplateTest) TestIngressComponentDisabled() {
+	// given
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"global.ingress.enabled": "true",
+			"operate.identity":       "false",
+			"operate.enabled":        "false",
+			"optimize.enabled":       "false",
+			"tasklist.enabled":       "false",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+		ExtraArgs:      map[string][]string{"template": {"--debug"}, "install": {"--debug"}},
+	}
+
+	// when
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+
+	// then
+	s.Require().NotContains(output, "name: camunda-platform-test-identity")
+	s.Require().NotContains(output, "name: camunda-platform-test-operate")
+	s.Require().NotContains(output, "name: camunda-platform-test-optimize")
+	s.Require().NotContains(output, "name: camunda-platform-test-tasklist")
 }
