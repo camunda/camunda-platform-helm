65b97869ef51c7131980a8955abe4b209068b8d1
Jan Friedenstab <8794084+jfriedenstab@users.noreply.github.com>
2023-05-04 14:28:15 +0200

    feat(web-modeler): add support for volume mounts to websockets deployment (#638)

--- charts/camunda-platform/test/unit/web-modeler/deployment_webapp_test.go
+++ charts/camunda-platform/test/unit/web-modeler/deployment_webapp_test.go
@@ -320,63 +320,6 @@ func (s *webappDeploymentTemplateTest) TestContainerShouldSetServerHttpsOnly() {
 	s.Require().Contains(env, corev1.EnvVar{Name: "SERVER_HTTPS_ONLY", Value: "true"})
 }
 
-func (s *webappDeploymentTemplateTest) TestContainerSetExtraVolumes() {
-	// given
-	options := &helm.Options{
-		SetValues: map[string]string{
-			"webModeler.enabled":                                      "true",
-			"webModeler.restapi.mail.fromAddress":                     "example@example.com",
-			"webModeler.webapp.extraVolumes[0].name":                  "extraVolume",
-			"webModeler.webapp.extraVolumes[0].configMap.name":        "otherConfigMap",
-			"webModeler.webapp.extraVolumes[0].configMap.defaultMode": "744",
-		},
-		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
-	}
-
-	// when
-	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
-	var deployment appsv1.Deployment
-	helm.UnmarshalK8SYaml(s.T(), output, &deployment)
-
-	// then
-	volumes := deployment.Spec.Template.Spec.Volumes
-	s.Require().Equal(1, len(volumes))
-
-	extraVolume := volumes[0]
-	s.Require().Equal("extraVolume", extraVolume.Name)
-	s.Require().NotNil(*extraVolume.ConfigMap)
-	s.Require().Equal("otherConfigMap", extraVolume.ConfigMap.Name)
-	s.Require().EqualValues(744, *extraVolume.ConfigMap.DefaultMode)
-}
-
-func (s *webappDeploymentTemplateTest) TestContainerSetExtraVolumeMounts() {
-	// given
-	options := &helm.Options{
-		SetValues: map[string]string{
-			"webModeler.enabled":                               "true",
-			"webModeler.restapi.mail.fromAddress":              "example@example.com",
-			"webModeler.webapp.extraVolumeMounts[0].name":      "otherConfigMap",
-			"webModeler.webapp.extraVolumeMounts[0].mountPath": "/usr/local/config",
-		},
-		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
-	}
-
-	// when
-	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
-	var deployment appsv1.Deployment
-	helm.UnmarshalK8SYaml(s.T(), output, &deployment)
-
-	// then
-	containers := deployment.Spec.Template.Spec.Containers
-	s.Require().Equal(1, len(containers))
-
-	volumeMounts := deployment.Spec.Template.Spec.Containers[0].VolumeMounts
-	s.Require().Equal(1, len(volumeMounts))
-	extraVolumeMount := volumeMounts[0]
-	s.Require().Equal("otherConfigMap", extraVolumeMount.Name)
-	s.Require().Equal("/usr/local/config", extraVolumeMount.MountPath)
-}
-
 func (s *webappDeploymentTemplateTest) TestContainerStartupProbe() {
 	// given
 	options := &helm.Options{
