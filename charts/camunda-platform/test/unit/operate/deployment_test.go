55a71cac7fd72da1e95148e1a2a5610acccbddc1
SvetlanaDorokhova <svetlana.dorokhova@camunda.com>
2023-09-25 19:53:06 +0200

    feat(operate): propagate global flag to Operate...
    to enable/disable multi-tenancy

--- charts/camunda-platform/test/unit/operate/deployment_test.go
+++ charts/camunda-platform/test/unit/operate/deployment_test.go
@@ -846,3 +846,22 @@ func (s *deploymentTemplateTest) TestInitContainers() {
 
 	s.Require().Contains(podContainers, expectedContainer)
 }
+
+func (s *deploymentTemplateTest) TestOperateMultiTenancyEnabled() {
+ 	// given
+ 	options := &helm.Options{
+ 		SetValues: map[string]string{
+ 			"global.multitenancy.enabled": "true",
+ 		},
+ 		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+ 	}
+
+ 	// when
+ 	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+ 	var statefulSet appsv1.StatefulSet
+ 	helm.UnmarshalK8SYaml(s.T(), output, &statefulSet)
+
+ 	// then
+ 	env := statefulSet.Spec.Template.Spec.Containers[0].Env
+ 	s.Require().Contains(env, corev1.EnvVar{Name: "CAMUNDA_OPERATE_MULTITENANCY_ENABLED", Value: "true"})
+}
