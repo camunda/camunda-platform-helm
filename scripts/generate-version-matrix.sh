ceeccb4ea3c9cb0f281692682a61a039a209193d
Jesse Simpson <jesse.simpson@camunda.com>
2024-02-26 13:01:21 -0500

    fix: include notice about optimize version in different helm versions

--- scripts/generate-version-matrix.sh
+++ scripts/generate-version-matrix.sh
@@ -7,6 +7,7 @@ set -euo pipefail
 CHART_VERSION="${CHART_VERSION:-latest}"
 CHART_SOURCE="${CHART_SOURCE:-charts/camunda-platform}"
 
+
 print_version_header () {
     echo "${OUTPUT_VERSION_HEADER:-"## Chart version $CHART_VERSION"}"
 }
@@ -19,6 +20,18 @@ get_chart_images () {
 }
 
 print_version_matrix_single () {
+  major=`echo $CHART_VERSION | cut -d. -f1`
+  minor=`echo $CHART_VERSION | cut -d. -f2`
+  patch=`echo $CHART_VERSION | cut -d. -f3`
+  if (( $major == 8 && $minor == 2 && $patch >= 23 )); then
+    OPTIMIZE_VERSION_NOTE="\n* The optimize image version is 8.2.x instead of 3.10.x because of a recent versioning change in optimize. In versions greater than 8.3.0, optimize's image will be identical to the other application components\n"
+  elif (( $major == 8 && $minor == 3 && $patch >= 2)); then
+    OPTIMIZE_VERSION_NOTE="\n* The optimize image version is uniform with the other components.\n"
+  elif (( $major == 9 )); then
+    OPTIMIZE_VERSION_NOTE="\n* The optimize image version is uniform with the other components.\n"
+  else
+    OPTIMIZE_VERSION_NOTE=""
+  fi
 cat << EOF
 $(print_version_header)
 
@@ -29,6 +42,7 @@ $(get_chart_images | grep "camunda")
 Non-Camunda images:
 
 $(get_chart_images | grep -v "camunda")
+$( printf "$OPTIMIZE_VERSION_NOTE" )
 
 EOF
 }
