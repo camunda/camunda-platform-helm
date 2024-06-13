1bd65ca58c56a821710532ed8fc6e68d97d492ca
Adam Urban <adam.urban@gmail.com>
2024-06-13 05:07:22 +0200

    feat: add console auth vars (#1782)
    * feat(console): add auth vars
    
    * feat(console): add auth vars
    
    * feat(console): add tests
    
    * fix(console): configmap test
    
    * fix(console): tests
    
    * fix(console): tests
    
    * fix(console): tests
    
    * fix(console): tests
    
    * update golden files
    
    ---------
    
    Co-authored-by: Ahmed AbouZaid <6760103+aabouzaid@users.noreply.github.com>

--- charts/camunda-platform-latest/test/unit/console/configmap_test.go
+++ charts/camunda-platform-latest/test/unit/console/configmap_test.go
@@ -19,9 +19,13 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
 )
 
 type configMapTemplateTest struct {
@@ -44,4 +48,31 @@ func TestConfigMapTemplate(t *testing.T) {
 		namespace: "camunda-platform-" + strings.ToLower(random.UniqueId()),
 		templates: []string{"templates/console/configmap.yaml"},
 	})
+}
+
+func (s *configMapTemplateTest) TestContainerShouldSetCorrectIdentityType() {
+	// given
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"console.enabled":                       "true",
+			"global.identity.auth.type":             "MICROSOFT",
+			"global.identity.auth.issuer":           "https://example.com",
+			"global.identity.auth.issuerBackendUrl": "https://example.com",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+	}
+
+	// when
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+	var configmap corev1.ConfigMap
+	var configmapApplication ConsoleYAML
+	helm.UnmarshalK8SYaml(s.T(), output, &configmap)
+
+	err := yaml.Unmarshal([]byte(configmap.Data["application.yaml"]), &configmapApplication)
+	if err != nil {
+		s.Fail("Failed to unmarshal yaml. error=", err)
+	}
+
+	// then
+	s.Require().Equal("MICROSOFT", configmapApplication.Camunda.Console.OAuth.Type)
 }
