b9fa7772c90a1f7d131a57875c4d05d42d692dc8
Daniel Rodriguez <daniel.rodriguez@camunda.com>
2024-06-21 16:10:04 -0400

    add unit test for console,connectors,identity,operate,optimze,tasklist

--- charts/camunda-platform-latest/test/unit/console/deployment_test.go
+++ charts/camunda-platform-latest/test/unit/console/deployment_test.go
@@ -744,3 +744,33 @@ func (s *deploymentTemplateTest) TestInitContainers() {
 
 	s.Require().Contains(podContainers, expectedContainer)
 }
+func (s *deploymentTemplateTest) TestSetDnsPolicyAndDnsConfig() {
+	// given
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"console.dnsPolicy":                "ClusterFirst",
+			"console.dnsConfig.nameservers[0]": "8.8.8.8",
+			"console.dnsConfig.searches[0]":    "example.com",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
+	}
+
+	// when
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
+	var deployment appsv1.Deployment
+	helm.UnmarshalK8SYaml(s.T(), output, &deployment)
+
+	// then
+	// Check if dnsPolicy is set
+	require.NotEmpty(s.T(), deployment.Spec.Template.Spec.DNSPolicy, "dnsPolicy should not be empty")
+
+	// Check if dnsConfig is set
+	require.NotNil(s.T(), deployment.Spec.Template.Spec.DNSConfig, "dnsConfig should not be nil")
+
+	expectedDNSConfig := &corev1.PodDNSConfig{
+		Nameservers: []string{"8.8.8.8"},
+		Searches:    []string{"example.com"},
+	}
+
+	require.Equal(s.T(), expectedDNSConfig, deployment.Spec.Template.Spec.DNSConfig, "dnsConfig should match the expected configuration")
+}
