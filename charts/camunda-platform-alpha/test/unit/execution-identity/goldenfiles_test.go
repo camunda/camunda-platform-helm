042411815552b18b5bfea785df434356979883f9
Daniel Rodriguez <daniel.rodriguez@camunda.com>
2024-08-21 13:43:47 -0400

    add goldenfiles_test.go

--- charts/camunda-platform-alpha/test/unit/execution-identity/goldenfiles_test.go
+++ charts/camunda-platform-alpha/test/unit/execution-identity/goldenfiles_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package executionIdentity
+package execution_identity
 
 import (
 	"camunda-platform/test/unit/utils"
