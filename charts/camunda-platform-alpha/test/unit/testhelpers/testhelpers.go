74ab7dd1c73d909d7bce58d7b16dabb7341cc36b
hisImminence <monma.immanuel@gmail.com>
2025-03-13 22:57:53 -0300

    test: support application.yaml config in generic unit test

--- charts/camunda-platform-alpha/test/unit/testhelpers/testhelpers.go
+++ charts/camunda-platform-alpha/test/unit/testhelpers/testhelpers.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -29,10 +30,18 @@ func RenderTemplate(t *testing.T, chartPath, release string, namespace string, t
 	return configmap
 }
 
-// VerifyConfigMap checks whether the generated ConfigMap contains expected key-value pairs
+// VerifyConfigMap checks whether the generated ConfigMap contains the expected key-value pairs
 func VerifyConfigMap(t *testing.T, testCase string, configmap corev1.ConfigMap, expectedValues map[string]string) {
 	for key, expectedValue := range expectedValues {
-		actualValue := strings.TrimSpace(configmap.Data[key])
+		var actualValue string
+		if strings.HasPrefix(key, "configmapApplication.") {
+            var configmapApplication map[string]interface{}
+            err := yaml.Unmarshal([]byte(configmap.Data["application.yaml"]), &configmapApplication)
+            require.NoError(t, err)
+            actualValue = getConfigMapFieldValue(configmapApplication, strings.Split(key, ".")[1:])
+        } else {
+            actualValue = strings.TrimSpace(configmap.Data[key])
+        }
 		require.Equal(t, expectedValue, actualValue, "Test case '%s': Expected key '%s' to have value '%s', but got '%s'", testCase, key, expectedValue, actualValue)
 	}
 }
@@ -45,4 +54,38 @@ func RunTestCases(t *testing.T, chartPath, release, namespace string, templates 
 			VerifyConfigMap(t, tc.Name, configmap, tc.Expected)
 		})
 	}
+}
+
+// getConfigMapFieldValue function traverses a nested map structure based on a given key path.
+// It handles maps with both interface{} and string keys, converting them as necessary to retrieve the desired value.
+// If the key is not found or the final value is not a string, the function returns an empty string.
+func getConfigMapFieldValue(configmapApplication map[string]interface{}, keyPath []string) string {
+	var current interface{} = configmapApplication
+
+	for _, key := range keyPath {
+        if nestedMap, ok := current.(map[interface{}]interface{}); ok {
+            // Convert map[interface{}]interface{} to map[string]interface{}
+            stringMap := make(map[string]interface{})
+            for k, v := range nestedMap {
+                if strKey, isString := k.(string); isString {
+                    stringMap[strKey] = v
+                }
+            }
+            // Move to the next level in the map
+            current = stringMap[key]
+        } else if nestedMap, ok := current.(map[string]interface{}); ok {
+            // If the current level is already a map with string keys, move to the next level
+            current = nestedMap[key]
+        } else {
+            // If the key is not found, return an empty string
+            return ""
+        }
+    }
+
+    // If the final value is a string, return it
+    if value, ok := current.(string); ok {
+        return value
+    }
+    // If the final value is not a string, return an empty string
+    return ""
 }
