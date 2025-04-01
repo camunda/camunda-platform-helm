c3d4ef6b4f3b8ed263ac07f415531c2caf94fc0e
Eamonn Moloney <eamonn.moloney@camunda.com>
2025-04-01 15:27:05 +0200

    fix: refactor to the new unit testing style
    Updated to the unit testing style and improvements to the testing helper

--- charts/camunda-platform-8.6/test/unit/camunda/ingress_test.go
+++ charts/camunda-platform-8.6/test/unit/camunda/ingress_test.go
@@ -24,6 +24,8 @@ import (
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	netv1 "k8s.io/api/networking/v1"
 )
 
