59a2b563581f713c24af1929f16d6c73e563445c
Eamonn Moloney <eamonn.moloney@camunda.com>
2025-04-01 11:13:31 +0200

    fix: Ingress unit tests refactored
    The ingress tests have been split into 2 files as there is a need for
    some tests to have nil template files. While this could be support via a
    flag in the testhelpers struct, it appears to be only interesting to 2
    test cases, so for this special case, a new file was created

--- charts/camunda-platform-8.6/test/unit/camunda/ingress_test.go
+++ charts/camunda-platform-8.6/test/unit/camunda/ingress_test.go
@@ -24,8 +24,6 @@ import (
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	netv1 "k8s.io/api/networking/v1"
 )
 
