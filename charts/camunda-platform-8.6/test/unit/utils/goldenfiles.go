5ae25b41bbce07673c94b70c27e9f269dd83a7cc
Eamonn Moloney <eamonn.moloney@camunda.com>
2025-04-02 08:36:07 +0200

    fix: task 448 refactor unit test in 8.6/test/unit/camunda directory (#3272)

--- charts/camunda-platform-8.6/test/unit/utils/goldenfiles.go
+++ charts/camunda-platform-8.6/test/unit/utils/goldenfiles.go
@@ -15,12 +15,12 @@
 package utils
 
 import (
+	"camunda-platform/test/unit/testhelpers"
 	"flag"
-	"io/ioutil"
+	"os"
 	"regexp"
+	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/helm"
-	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -38,31 +38,35 @@ type TemplateGoldenTest struct {
 	ExtraHelmArgs  []string
 }
 
-func (s *TemplateGoldenTest) TestContainerGoldenTestDefaults() {
-	options := &helm.Options{
-		KubectlOptions: k8s.NewKubectlOptions("", "", s.Namespace),
-		SetValues:      s.SetValues,
+func (s *TemplateGoldenTest) TestDifferentValuesInputs() {
+	testCases := []testhelpers.TestCase{
+		{
+			Name:                    "TestContainerGoldenTestDefaults",
+			Values:                  s.SetValues,
+			RenderTemplateExtraArgs: s.ExtraHelmArgs,
+			Verifier: func(t *testing.T, output string, err error) {
+				s.IgnoredLines = append(s.IgnoredLines, `\s+helm.sh/chart:\s+.*`)
+				bytes := []byte(output)
+				for _, ignoredLine := range s.IgnoredLines {
+					regex := regexp.MustCompile(ignoredLine)
+					bytes = regex.ReplaceAll(bytes, []byte(""))
+				}
+				output = string(bytes)
+
+				goldenFile := "golden/" + s.GoldenFileName + ".golden.yaml"
+
+				if *update {
+					err := os.WriteFile(goldenFile, bytes, 0644)
+					s.Require().NoError(err, "Golden file was not writable")
+				}
+
+				expected, e := os.ReadFile(goldenFile)
+
+				// then
+				s.Require().NoError(e, "Golden file doesn't exist or was not readable")
+				s.Require().Equal(string(expected), output)
+			},
+		},
 	}
-	output := helm.RenderTemplate(s.T(), options, s.ChartPath, s.Release, s.Templates, s.ExtraHelmArgs...)
-
-	s.IgnoredLines = append(s.IgnoredLines, `\s+helm.sh/chart:\s+.*`)
-	bytes := []byte(output)
-	for _, ignoredLine := range s.IgnoredLines {
-		regex := regexp.MustCompile(ignoredLine)
-		bytes = regex.ReplaceAll(bytes, []byte(""))
-	}
-	output = string(bytes)
-
-	goldenFile := "golden/" + s.GoldenFileName + ".golden.yaml"
-
-	if *update {
-		err := ioutil.WriteFile(goldenFile, bytes, 0644)
-		s.Require().NoError(err, "Golden file was not writable")
-	}
-
-	expected, err := ioutil.ReadFile(goldenFile)
-
-	// then
-	s.Require().NoError(err, "Golden file doesn't exist or was not readable")
-	s.Require().Equal(string(expected), output)
+	testhelpers.RunTestCasesE(s.T(), s.ChartPath, s.Release, s.Namespace, s.Templates, testCases)
 }
