1defa03b6c55c3c67a8a191b15ef92b3a39c8f82
Eamonn Moloney <eamonn.moloney@camunda.com>
2025-04-07 10:02:26 +0100

    fix: refactor of zeebe unit tests in 8.6

--- charts/camunda-platform-8.6/test/unit/zeebe/service_test.go
+++ charts/camunda-platform-8.6/test/unit/zeebe/service_test.go
@@ -15,19 +15,19 @@
 package zeebe
 
 import (
+	"camunda-platform/test/unit/testhelpers"
 	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/helm"
-	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	coreV1 "k8s.io/api/core/v1"
 )
 
-type serviceTest struct {
+type ServiceTest struct {
 	suite.Suite
 	chartPath string
 	release   string
@@ -41,7 +41,7 @@ func TestServiceTemplate(t *testing.T) {
 	chartPath, err := filepath.Abs("../../../")
 	require.NoError(t, err)
 
-	suite.Run(t, &serviceTest{
+	suite.Run(t, &ServiceTest{
 		chartPath: chartPath,
 		release:   "camunda-platform-test",
 		namespace: "camunda-platform-" + strings.ToLower(random.UniqueId()),
@@ -49,49 +49,56 @@ func TestServiceTemplate(t *testing.T) {
 	})
 }
 
-func (s *serviceTest) TestContainerSetGlobalAnnotations() {
-	// given
-	options := &helm.Options{
-		SetValues: map[string]string{
-			"global.annotations.foo": "bar",
+func (s *ServiceTest) TestDifferentValuesInputs() {
+	testCases := []testhelpers.TestCase{
+		{
+			Name: "TestContainerSetGlobalAnnotations",
+			Values: map[string]string{
+				"global.annotations.foo": "bar",
+			},
+			Verifier: func(t *testing.T, output string, err error) {
+				var service coreV1.Service
+				helm.UnmarshalK8SYaml(s.T(), output, &service)
+
+				// then
+				s.Require().Equal("bar", service.ObjectMeta.Annotations["foo"])
+			},
+		}, {
+			Name: "TestExtraPorts",
+			Values: map[string]string{
+				"zeebe.service.extraPorts[0].name":       "hazelcast",
+				"zeebe.service.extraPorts[0].protocol":   "TCP",
+				"zeebe.service.extraPorts[0].port":       "5701",
+				"zeebe.service.extraPorts[0].targetPort": "5701",
+			},
+			Verifier: func(t *testing.T, output string, err error) {
+				var service coreV1.Service
+				helm.UnmarshalK8SYaml(s.T(), output, &service)
+
+				// then
+				expectedPort := int32(5701)
+				expectedName := "hazelcast"
+				expectedTargetPort := int32(5701)
+				ports := service.Spec.Ports
+
+				s.Require().Equal(expectedPort, ports[3].Port)
+				s.Require().Equal(expectedName, ports[3].Name)
+				s.Require().Equal(expectedTargetPort, ports[3].TargetPort.IntVal)
+			},
+		}, {
+			Name: "TestContainerServiceAnnotations",
+			Values: map[string]string{
+				"zeebe.service.annotations.foo": "bar",
+			},
+			Verifier: func(t *testing.T, output string, err error) {
+				var service coreV1.Service
+				helm.UnmarshalK8SYaml(s.T(), output, &service)
+
+				// then
+				s.Require().Equal("bar", service.ObjectMeta.Annotations["foo"])
+			},
 		},
-		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
 	}
 
-	// when
-	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
-	var service coreV1.Service
-	helm.UnmarshalK8SYaml(s.T(), output, &service)
-
-	// then
-	s.Require().Equal("bar", service.ObjectMeta.Annotations["foo"])
-}
-
-func (s *serviceTest) TestExtraPorts() {
-	// given
-	options := &helm.Options{
-		SetValues: map[string]string{
-			"zeebe.service.extraPorts[0].name":       "hazelcast",
-			"zeebe.service.extraPorts[0].protocol":   "TCP",
-			"zeebe.service.extraPorts[0].port":       "5701",
-			"zeebe.service.extraPorts[0].targetPort": "5701",
-		},
-		KubectlOptions: k8s.NewKubectlOptions("", "", s.namespace),
-	}
-
-	// when
-	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.release, s.templates)
-	var service coreV1.Service
-	helm.UnmarshalK8SYaml(s.T(), output, &service)
-
-	// then
-	expectedPort := int32(5701)
-	expectedName := "hazelcast"
-	expectedTargetPort := int32(5701)
-	ports := service.Spec.Ports
-
-	s.Require().Equal(expectedPort, ports[3].Port)
-	s.Require().Equal(expectedName, ports[3].Name)
-	s.Require().Equal(expectedTargetPort, ports[3].TargetPort.IntVal)
-
+	testhelpers.RunTestCasesE(s.T(), s.chartPath, s.release, s.namespace, s.templates, testCases)
 }
