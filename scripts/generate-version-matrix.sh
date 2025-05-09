65b7a58d5f88d28aa7853397cb1626d445d43884
Jesse Simpson <jesse.simpson@camunda.com>
2025-05-09 09:28:59 -0400

    fix: adjust scripts with change from name ingress to ingress-base

--- scripts/generate-version-matrix.sh
+++ scripts/generate-version-matrix.sh
@@ -53,7 +53,7 @@ get_chart_images () {
       helm repo update > /dev/null
       chart_images="$(
         helm template --skip-tests camunda "${CHART_SOURCE}" --version "${chart_version}" \
-          --values "${CHART_DIR}/test/integration/scenarios/chart-full-setup/values-integration-test-ingress.yaml" 2> /dev/null |
+          --values "${CHART_DIR}/test/integration/scenarios/chart-full-setup/values-integration-test-ingress-base.yaml" 2> /dev/null |
         tr -d "\"'" | awk '/image:/{gsub(/^(camunda|bitnami)/, "docker.io/&", $2); printf "%s\n", $2}' |
         sort | uniq;
       )"
