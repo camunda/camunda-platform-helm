600b93b3d6725cb5e7d039317a5eb612ed4321c3
hamza-m-masood <hamzamasood183@gmail.com>
2025-06-25 19:33:32 +0100

    feat: enhance integration test setup with new auth scenarios
    Added new YAML files for integration tests to support different
    authentication scenarios, including OIDC and Keycloak. Updated
    workflow and task files to accommodate these changes. Commented
    out the core test suite temporarily and adjusted the identifier
    format in workflows.

--- scripts/list-chart-images.sh
+++ scripts/list-chart-images.sh
@@ -6,5 +6,5 @@ set -euo pipefail
 #
 
 helm template --skip-tests camunda "${CHART_SOURCE}" --version "${CHART_VERSION}" \
-    --values "${CHART_VALUES_DIR}test/integration/scenarios/chart-full-setup/values-integration-test-ingress-base.yaml" 2> /dev/null |
+    --values "${CHART_VALUES_DIR}test/integration/scenarios/chart-full-setup/values-integration-test-ingress-keycloak.yaml" 2> /dev/null |
         tr -d "\"'" | awk '/image:/{gsub(/^(camunda|bitnami)/, "docker.io/&", $2); printf "- %s\n", $2}'
