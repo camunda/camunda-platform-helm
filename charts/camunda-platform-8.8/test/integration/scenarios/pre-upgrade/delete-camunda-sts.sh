e464174c0ae72a972d5eca6522d293b555528617
Ahmed AbouZaid <6760103+aabouzaid@users.noreply.github.com>
2025-08-20 02:39:51 +0200

    ci: make the pre-scripts more visible

--- charts/camunda-platform-8.8/test/integration/scenarios/pre-upgrade/delete-camunda-sts.sh
+++
@@ -1,6 +0,0 @@
-# Delete the StatefulSet of Camunda Orchestration cluster as the "matchLabels" changed
-# after the rename from "core" to "orchestration".
-kubectl delete sts -n "${TEST_NAMESPACE}" -l app.kubernetes.io/component=core --ignore-not-found
-# Adding "orchestration" also, as this will change again before the final release, to avoid the need to delete the StatefulSet.
-# It will be again "zeebe-broker" as the 8.7 release.
-kubectl delete sts -n "${TEST_NAMESPACE}" -l app.kubernetes.io/component=orchestration --ignore-not-found
