2da03c02d455522ca022f254e0e3d8a4526c1540
Eamonn Moloney <eamonn.moloney@camunda.com>
2025-08-29 12:19:04 +0100

    fix

--- scripts/base_playwright_script.sh
+++ scripts/base_playwright_script.sh
@@ -88,7 +88,7 @@ run_playwright_tests() {
   cd "$test_suite_path" || exit
 
   npm i --no-audit --no-fund --silent
-  sudo npx playwright install-deps
+  #  sudo npx playwright install-deps
   npx playwright install
 
   if [[ $show_html_report == "true" ]]; then
