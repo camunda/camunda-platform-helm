1513cbcfac396fefc75049d83fb1db40eff9c679
Ahmed AbouZaid <6760103+aabouzaid@users.noreply.github.com>
2025-09-20 05:11:56 +0200

    test: automatically clean up golden files 8.7

--- charts/camunda-platform-8.7/test/unit/optimize/goldenfiles_test.go
+++ charts/camunda-platform-8.7/test/unit/optimize/goldenfiles_test.go
@@ -17,32 +17,20 @@ package optimize
 import (
 	"camunda-platform/test/unit/utils"
 	"path/filepath"
-	"strings"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 )
 
 func TestGoldenDefaultsTemplate(t *testing.T) {
-	t.Parallel()
-
 	chartPath, err := filepath.Abs("../../../")
 	require.NoError(t, err)
 	templateNames := []string{"service", "serviceaccount", "deployment"}
 
-	for _, name := range templateNames {
-		suite.Run(t, &utils.TemplateGoldenTest{
-			ChartPath:      chartPath,
-			Release:        "camunda-platform-test",
-			Namespace:      "camunda-platform-" + strings.ToLower(random.UniqueId()),
-			GoldenFileName: name,
-			Templates:      []string{"templates/optimize/" + name + ".yaml"},
-			IgnoredLines: []string{
-				`\s+.*-secret:\s+.*`,    // secrets are auto-generated and need to be ignored.
-				`\s+checksum/.+?:\s+.*`, // ignore configmap checksum.
-			},
-		})
+	ignoredLines := []string{
+		`\s+.*-secret:\s+.*`,    // secrets are auto-generated and need to be ignored.
+		`\s+checksum/.+?:\s+.*`, // ignore configmap checksum.
 	}
+
+	utils.TestGoldenTemplates(t, chartPath, "optimize", templateNames, ignoredLines, nil, nil)
 }
