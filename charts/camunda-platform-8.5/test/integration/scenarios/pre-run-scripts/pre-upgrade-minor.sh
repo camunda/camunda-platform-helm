2ca69b0ff9149a5b980129956a12f5407951d287
Ahmed AbouZaid <6760103+aabouzaid@users.noreply.github.com>
2025-09-27 04:50:47 +0200

    wip 13

--- charts/camunda-platform-8.5/test/integration/scenarios/pre-run-scripts/pre-upgrade-minor.sh
+++ charts/camunda-platform-8.5/test/integration/scenarios/pre-run-scripts/pre-upgrade-minor.sh
@@ -4,5 +4,7 @@
 # Any necessary tasks should be performed here and removed after the release.
 #
 
+set -x
+
 # Delete the Identity deployment as we moved Identity as part of the main chart instead of a subchart.
 kubectl delete deployment -n "${TEST_NAMESPACE}" -l app.kubernetes.io/component=identity --ignore-not-found
--- charts/camunda-platform-8.6/test/integration/scenarios/pre-run-scripts/pre-upgrade-minor.sh
+++ charts/camunda-platform-8.6/test/integration/scenarios/pre-run-scripts/pre-upgrade-minor.sh
@@ -3,3 +3,5 @@
 # This script will run before the Camunda Helm chart upgrade step in the "upgrade-minor" flow.
 # Any necessary tasks should be performed here and removed after the release.
 #
+
+set -x
