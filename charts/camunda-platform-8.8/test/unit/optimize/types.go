1ec9594ece20d508150afdbe01d6902f97f36948
Jesse Simpson <jesse.simpson@camunda.com>
2025-12-17 15:32:12 -0500

    test: port to 8.8
    Signed-off-by: Jesse Simpson <jesse.simpson@camunda.com>

--- charts/camunda-platform-8.8/test/unit/optimize/types.go
+++ charts/camunda-platform-8.8/test/unit/optimize/types.go
@@ -3,6 +3,7 @@ package optimize
 type OptimizeConfigYAML struct {
 	Container ContainerYAML `yaml:"container"`
 	Zeebe     ZeebeYAML     `yaml:"zeebe"`
+	Multitenancy MultitenancyYAML `yaml:"multitenancy"`
 }
 
 type ContainerYAML struct {
@@ -11,4 +12,9 @@ type ContainerYAML struct {
 
 type ZeebeYAML struct {
 	Name string `yaml:"name"`
+}
+
+
+type MultitenancyYAML struct {
+	Enabled bool `yaml:"enabled"`
 }
