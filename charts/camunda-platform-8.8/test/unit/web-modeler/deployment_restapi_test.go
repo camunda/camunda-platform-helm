2c896c27b4b2a974a0c74418e43756e9d12769c1
Jesse Simpson <jesse.simpson@camunda.com>
2025-12-30 16:42:52 -0500

    test: unit tests added
    Signed-off-by: Jesse Simpson <jesse.simpson@camunda.com>

--- charts/camunda-platform-8.8/test/unit/web-modeler/deployment_restapi_test.go
+++ charts/camunda-platform-8.8/test/unit/web-modeler/deployment_restapi_test.go
@@ -474,3 +474,67 @@ func (s *RestapiDeploymentTemplateTest) TestDifferentValuesInputs() {
 
 	testhelpers.RunTestCasesE(s.T(), s.chartPath, s.release, s.namespace, s.templates, testCases)
 }
+
+func (s *RestapiDeploymentTemplateTest) TestSpringConfigImport() {
+	testCases := []testhelpers.TestCase{
+		{
+			Name: "TestSpringConfigImportDefault",
+			Values: map[string]string{
+				"identity.enabled":                    "true",
+				"webModeler.enabled":                  "true",
+				"webModeler.restapi.mail.fromAddress": "example@example.com",
+			},
+			Verifier: func(t *testing.T, output string, err error) {
+				var deployment appsv1.Deployment
+				helm.UnmarshalK8SYaml(s.T(), output, &deployment)
+
+				// then - SPRING_CONFIG_IMPORT should be set with default value
+				env := deployment.Spec.Template.Spec.Containers[0].Env
+				s.Require().Contains(env, corev1.EnvVar{
+					Name:  "SPRING_CONFIG_IMPORT",
+					Value: "optional:file:/home/runner/config/additional-spring-properties.yaml",
+				}, "SPRING_CONFIG_IMPORT should be set with default value")
+			},
+		}, {
+			Name: "TestSpringConfigImportCustomValue",
+			Values: map[string]string{
+				"identity.enabled":                      "true",
+				"webModeler.enabled":                    "true",
+				"webModeler.restapi.mail.fromAddress":   "example@example.com",
+				"webModeler.restapi.springConfigImport": "optional:file:/home/runner/config/custom-config.yaml",
+			},
+			Verifier: func(t *testing.T, output string, err error) {
+				var deployment appsv1.Deployment
+				helm.UnmarshalK8SYaml(s.T(), output, &deployment)
+
+				// then - SPRING_CONFIG_IMPORT should be set with custom value
+				env := deployment.Spec.Template.Spec.Containers[0].Env
+				s.Require().Contains(env, corev1.EnvVar{
+					Name:  "SPRING_CONFIG_IMPORT",
+					Value: "optional:file:/home/runner/config/custom-config.yaml",
+				}, "SPRING_CONFIG_IMPORT should be set with custom value")
+			},
+		}, {
+			Name: "TestSpringConfigImportEmpty",
+			Values: map[string]string{
+				"identity.enabled":                      "true",
+				"webModeler.enabled":                    "true",
+				"webModeler.restapi.mail.fromAddress":   "example@example.com",
+				"webModeler.restapi.springConfigImport": "",
+			},
+			Verifier: func(t *testing.T, output string, err error) {
+				var deployment appsv1.Deployment
+				helm.UnmarshalK8SYaml(s.T(), output, &deployment)
+
+				// then - SPRING_CONFIG_IMPORT should NOT be present when empty
+				env := deployment.Spec.Template.Spec.Containers[0].Env
+				for _, envvar := range env {
+					s.Require().NotEqual("SPRING_CONFIG_IMPORT", envvar.Name,
+						"SPRING_CONFIG_IMPORT should not be present when empty")
+				}
+			},
+		},
+	}
+
+	testhelpers.RunTestCasesE(s.T(), s.chartPath, s.release, s.namespace, s.templates, testCases)
+}
