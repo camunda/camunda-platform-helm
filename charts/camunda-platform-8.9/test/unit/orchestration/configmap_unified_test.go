b1033dc2dead426ef64c6476e5f77003e58798bb
Jesse Simpson <jesse.simpson@camunda.com>
2026-02-19 10:14:49 -0500

    fix: add back username and url
    Signed-off-by: Jesse Simpson <jesse.simpson@camunda.com>

--- charts/camunda-platform-8.9/test/unit/orchestration/configmap_unified_test.go
+++ charts/camunda-platform-8.9/test/unit/orchestration/configmap_unified_test.go
@@ -295,8 +295,23 @@ func (s *ConfigmapTemplateTest) TestDifferentValuesInputsUnifiedAuthOIDC() {
 func (s *ConfigmapTemplateTest) TestDifferentValuesInputsUnifiedRDBMS() {
 	testCases := []testhelpers.TestCase{
 		{
+			Name: "TestApplicationYamlShouldContainRDBMSBasicConfig",
+			Values: map[string]string{
+				"orchestration.data.secondaryStorage.rdbms.url":                 "jdbc:postgresql://localhost:5432/camunda",
+				"orchestration.data.secondaryStorage.rdbms.username":            "camunda",
+				"orchestration.data.secondaryStorage.rdbms.secret.inlineSecret": "my-password",
+			},
+			Expected: map[string]string{
+				"configmapApplication.camunda.data.secondary-storage.rdbms.url":      "jdbc:postgresql://localhost:5432/camunda",
+				"configmapApplication.camunda.data.secondary-storage.rdbms.username": "camunda",
+				"configmapApplication.camunda.data.secondary-storage.rdbms.password": "${VALUES_ORCHESTRATION_DATA_SECONDARYSTORAGE_RDBMS_PASSWORD:}",
+			},
+		},
+		{
 			Name: "TestApplicationYamlShouldContainRDBMSPasswordWithInlineSecret",
 			Values: map[string]string{
+				"orchestration.data.secondaryStorage.rdbms.url":                 "jdbc:postgresql://localhost:5432/camunda",
+				"orchestration.data.secondaryStorage.rdbms.username":            "camunda",
 				"orchestration.data.secondaryStorage.rdbms.secret.inlineSecret": "my-password",
 			},
 			Expected: map[string]string{
@@ -306,6 +321,8 @@ func (s *ConfigmapTemplateTest) TestDifferentValuesInputsUnifiedRDBMS() {
 		{
 			Name: "TestApplicationYamlShouldContainRDBMSPasswordWithExistingSecret",
 			Values: map[string]string{
+				"orchestration.data.secondaryStorage.rdbms.url":                      "jdbc:postgresql://localhost:5432/camunda",
+				"orchestration.data.secondaryStorage.rdbms.username":                 "camunda",
 				"orchestration.data.secondaryStorage.rdbms.secret.existingSecret":    "my-secret",
 				"orchestration.data.secondaryStorage.rdbms.secret.existingSecretKey": "password",
 			},
