29b2ff42dfce32ad58d016d72e9e05b72043a913
Jesse Simpson <jesse.simpson@camunda.com>
2026-03-12 12:22:34 -0400

    fix: did not mean to commit this
    Signed-off-by: Jesse Simpson <jesse.simpson@camunda.com>

--- scripts/delete_orphaned_indexes/delete_orphaned_indices.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-indices="$( ./scripts/delete_orphaned_indexes/find-orphaned-es-indexes.sh )"
-parallel --will-cite -j30 ./scripts/delete_orphaned_indexes/sendrequest.sh ::: $indices
--- scripts/delete_orphaned_indexes/sendrequest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-response=$(curl -s -o /dev/null -w "%{http_code}" ${ES_AUTH} -X DELETE "${ES_URL}/$1")
-
-if [ "$response" -eq 200 ]; then
-  echo "  -> Deleted successfully"
-else
-  echo "  -> FAILED (HTTP ${response})"
-fi
