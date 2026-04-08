50fad4824fa3d3c7d497677578db7be2b9927b54
Hamza Masood <47217263+hamza-m-masood@users.noreply.github.com>
2026-04-08 18:19:31 +0100

    docs: bootstrap Docusaurus site and CI workflows (#5465)

---
+++ helm-docs-site/sidebars.js
@@ -0,0 +1,44 @@
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  docs: [
+    {
+      type: 'doc',
+      id: 'index',
+      label: 'Overview',
+    },
+    {
+      type: 'doc',
+      id: 'release-process',
+      label: 'Release Process',
+    },
+    {
+      type: 'doc',
+      id: 'github-actions-workflows',
+      label: 'GitHub Actions Workflows',
+    },
+    {
+      type: 'doc',
+      id: 'contribution-and-collaboration',
+      label: 'Contribution & Collaboration',
+    },
+    {
+      type: 'doc',
+      id: 'code-style',
+      label: 'Code Style',
+    },
+    {
+      type: 'doc',
+      id: 'breaking-changes',
+      label: 'Breaking Changes & Deprecation Policy',
+    },
+    {
+      type: 'doc',
+      id: 'ticket-and-label-policy',
+      label: 'Ticket & Label Policy',
+    },
+  ],
+};
+
+module.exports = sidebars;
