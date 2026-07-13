6ca3bc0c496f4bf773dc76cf105cdbe570f19b1d
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2026-07-13 18:47:47 +0200

    fix: allow Web Modeler and Console with external management Identity

--- charts/camunda-platform-8.10/test/unit/common/constraints_test.go
+++ charts/camunda-platform-8.10/test/unit/common/constraints_test.go
@@ -602,3 +602,35 @@ func (s *ConstraintTemplateTest) TestDeprecatedKeyHelperRendersWithoutCrash() {
 
 	testhelpers.RunTestCasesE(s.T(), s.chartPath, s.release, s.namespace, s.templates, testCases)
 }
+
+func (s *ConstraintTemplateTest) TestManagementIdentityExternalServiceUrl() {
+	testCases := []testhelpers.TestCase{
+		{
+			Name: "WebModelerWithExternalManagementIdentityRenders",
+			Values: map[string]string{
+				"orchestration.enabled":               "false",
+				"webModeler.enabled":                  "true",
+				"webModeler.restapi.mail.fromAddress": "test@example.com",
+				"identity.enabled":                    "false",
+				"global.identity.service.url":         "http://identity.other-ns.svc:8080",
+			},
+			Verifier: func(t *testing.T, output string, err error) {
+				s.Require().NoError(err)
+			},
+		},
+		{
+			Name: "WebModelerWithoutManagementIdentityFails",
+			Values: map[string]string{
+				"orchestration.enabled":               "false",
+				"webModeler.enabled":                  "true",
+				"webModeler.restapi.mail.fromAddress": "test@example.com",
+				"identity.enabled":                    "false",
+			},
+			Verifier: func(t *testing.T, output string, err error) {
+				s.Require().ErrorContains(err, "Web Modeler is enabled but management Identity is not configured")
+			},
+		},
+	}
+
+	testhelpers.RunTestCasesE(s.T(), s.chartPath, s.release, s.namespace, s.templates, testCases)
+}
