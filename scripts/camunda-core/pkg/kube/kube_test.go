1336bf2e74356b1025d3bd396b0acc6338961b37
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2026-07-24 11:16:35 +0200

    chore: centralize readiness route discovery

--- scripts/camunda-core/pkg/kube/kube_test.go
+++ scripts/camunda-core/pkg/kube/kube_test.go
@@ -1,3 +1,17 @@
+// Copyright 2026 Camunda Services GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package kube
 
 import (
@@ -11,6 +25,44 @@ import (
 	dynamicfake "k8s.io/client-go/dynamic/fake"
 	k8stesting "k8s.io/client-go/testing"
 )
+
+func TestListNamespacedResources(t *testing.T) {
+	resource := schema.GroupVersionResource{Group: "gateway.networking.k8s.io", Version: "v1", Resource: "httproutes"}
+	route := &unstructured.Unstructured{Object: map[string]any{
+		"apiVersion": "gateway.networking.k8s.io/v1",
+		"kind":       "HTTPRoute",
+		"metadata": map[string]any{
+			"name":      "integration-route",
+			"namespace": "test-namespace",
+		},
+	}}
+	dynamicClient := dynamicfake.NewSimpleDynamicClientWithCustomListKinds(
+		runtime.NewScheme(),
+		map[schema.GroupVersionResource]string{resource: "HTTPRouteList"},
+		route,
+	)
+
+	client := &Client{dynamicClient: dynamicClient}
+	resources, err := client.ListNamespacedResources(context.Background(), "test-namespace", resource)
+	if err != nil {
+		t.Fatalf("ListNamespacedResources() error = %v", err)
+	}
+	if len(resources.Items) != 1 || resources.Items[0].GetName() != "integration-route" {
+		t.Fatalf("ListNamespacedResources() items = %#v", resources.Items)
+	}
+}
+
+func TestListNamespacedResourcesRejectsEmptyNamespace(t *testing.T) {
+	client := &Client{dynamicClient: dynamicfake.NewSimpleDynamicClient(runtime.NewScheme())}
+	_, err := client.ListNamespacedResources(
+		context.Background(),
+		"",
+		schema.GroupVersionResource{Group: "networking.k8s.io", Version: "v1", Resource: "ingresses"},
+	)
+	if err == nil {
+		t.Fatal("ListNamespacedResources() expected an error for an empty namespace")
+	}
+}
 
 func TestCheckConnectivity_InvalidContext(t *testing.T) {
 	err := CheckConnectivity(context.Background(), "nonexistent-context-that-should-not-exist")
