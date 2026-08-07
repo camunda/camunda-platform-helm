22c80d864140cd971bc33dd7939a40a7a395c91e
Eamonn Moloney <eamonn.moloney@camunda.com>
2026-08-07 12:23:49 +0100

    test(8.10): remove misleading warning test comments

--- charts/camunda-platform-8.10/test/unit/common/configmap_warnings_test.go
+++ charts/camunda-platform-8.10/test/unit/common/configmap_warnings_test.go
@@ -87,9 +87,6 @@ func (s *ConfigMapWarningsTemplateTest) TestDifferentValuesInputs() {
 	testhelpers.RunTestCasesE(s.T(), s.chartPath, s.release, s.namespace, s.templates, testCases)
 }
 
-// TestConsoleConfigKeysWarningRendersInConfigMap asserts that setting a
-// non-"enabled" console.* key renders the consolidated console consolidation
-// warning into the "-warnings" ConfigMap.
 func (s *ConfigMapWarningsTemplateTest) TestConsoleConfigKeysWarningRendersInConfigMap() {
 	testCases := []testhelpers.TestCase{
 		{
@@ -114,10 +111,6 @@ func (s *ConfigMapWarningsTemplateTest) TestConsoleConfigKeysWarningRendersInCon
 	testhelpers.RunTestCasesE(s.T(), s.chartPath, s.release, s.namespace, s.templates, testCases)
 }
 
-// TestGlobalIdentityAuthConsoleDeprecationWarningRendersInConfigMap asserts that
-// setting any child under global.identity.auth.console renders the
-// "no replacement" removal warning into the "-warnings" ConfigMap, and that no
-// migration target is suggested for that subtree.
 func (s *ConfigMapWarningsTemplateTest) TestGlobalIdentityAuthConsoleDeprecationWarningRendersInConfigMap() {
 	testCases := []testhelpers.TestCase{
 		{
