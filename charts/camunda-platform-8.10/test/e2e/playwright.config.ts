c45eb8d3f8b5313d780469cf74af4a490cc4b995
Eamonn Moloney <eamonn.moloney@camunda.com>
2026-09-23 07:24:29 +0100

    test(8.10): append physical tenant project to smoke

--- charts/camunda-platform-8.10/test/e2e/playwright.config.ts
+++ charts/camunda-platform-8.10/test/e2e/playwright.config.ts
@@ -82,13 +82,7 @@ export default defineConfig(
     workers: "100%",
     extraProjects: [
       {
-        name: "topology-orchestration-local",
-        testDir: __dirname,
-        testMatch: ["topology-orchestration.spec.ts"],
-      },
-      {
         name: "physical-tenants",
-        dependencies: ["topology-orchestration-local"],
         testDir: __dirname,
         testMatch: ["physical-tenants.spec.ts"],
       },
